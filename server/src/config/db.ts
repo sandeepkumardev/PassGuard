d4cbc7d8ab3a117e1c79e2b33544d8a02d9471c6
sandeep <sandeepypb@gmail.com>
2024-01-29 18:51:46 +0530

    🐛 Fix: add DB host

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize";
 
 interface DBConig {
+  host: string;
   name: string;
   username: string;
   password: string;
@@ -8,18 +9,18 @@ interface DBConig {
 }
 
 const db_Config: DBConig = {
+  host: process.env.DB_HOST || "",
   name: process.env.DB_NAME || "",
   username: process.env.DB_USERNAME || "",
   password: process.env.DB_PASSWORD || "",
   dialect: process.env.DB_DIALECT || "",
 };
 
-export const sequelize = new Sequelize(
-  db_Config.name,
-  db_Config.username,
-  db_Config.password,
-  {
-    dialect: db_Config.dialect,
-    logging: false,
-  }
-);
+export const sequelize = new Sequelize({
+  host: db_Config.host,
+  database: db_Config.name,
+  username: db_Config.username,
+  password: db_Config.password,
+  dialect: db_Config.dialect,
+  logging: false,
+});
